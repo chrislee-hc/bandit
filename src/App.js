87b54f6ff9f77b85001920cf4a79bbb6d6de27dc
Christopher Lee <christopherlee@college.harvard.edu>
2022-11-29 21:01:23 -0500

    test build

--- src/App.js
+++ src/App.js
@@ -6,7 +6,13 @@ function App() {
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    const newSocket = io(`http://${window.location.hostname}:3000`);
+    const USE_PROD_SERVER = true;
+    let newSocket;
+    if (USE_PROD_SERVER) {
+      newSocket = io("https://bandit-server-production.up.railway.app/");
+    } else {
+      newSocket = io(`http://${window.location.hostname}:3000`);
+    }
     setSocket(newSocket);
     return () => newSocket.close();
   }, [setSocket]);
